e0c962c24bcc66bc25f7f07c485f132d91394456
Mark Vujevits <vujevits.mark@gmail.com>
2026-05-03 15:55:33 +0200

    update game

--- public/guard-game/ccip-BufM3hgj.js
+++ public/guard-game/ccip-BufM3hgj.js
@@ -1,30 +1,30 @@
-import { Ft as e, It as t, Kn as n, Mt as r, Rt as i, jn as a, l as o, qn as s, r as c, t as l, wt as u } from "./localBatchGatewayRequest-CHS18WUu.js";
+import { Ft as e, It as t, Jn as n, Mn as r, Mt as i, Rt as a, l as o, qn as s, r as c, t as l, wt as u } from "./localBatchGatewayRequest-BxBnYHr3.js";
 //#region node_modules/.pnpm/viem@2.47.6_typescript@5.9.3/node_modules/viem/_esm/errors/ccip.js
-var d = class extends s {
-	constructor({ callbackSelector: e, cause: t, data: n, extraData: i, sender: a, urls: o }) {
+var d = class extends n {
+	constructor({ callbackSelector: e, cause: t, data: n, extraData: r, sender: a, urls: o }) {
 		super(t.shortMessage || "An error occurred while fetching for an offchain result.", {
 			cause: t,
 			metaMessages: [
 				...t.metaMessages || [],
 				t.metaMessages?.length ? "" : [],
 				"Offchain Gateway Call:",
-				o && ["  Gateway URL(s):", ...o.map((e) => `    ${r(e)}`)],
+				o && ["  Gateway URL(s):", ...o.map((e) => `    ${i(e)}`)],
 				`  Sender: ${a}`,
 				`  Data: ${n}`,
 				`  Callback selector: ${e}`,
-				`  Extra data: ${i}`
+				`  Extra data: ${r}`
 			].flat(),
 			name: "OffchainLookupError"
 		});
 	}
-}, f = class extends s {
+}, f = class extends n {
 	constructor({ result: t, url: n }) {
 		super("Offchain gateway response is malformed. Response data must be a hex value.", {
-			metaMessages: [`Gateway URL: ${r(n)}`, `Response: ${e(t)}`],
+			metaMessages: [`Gateway URL: ${i(n)}`, `Response: ${e(t)}`],
 			name: "OffchainLookupResponseMalformedError"
 		});
 	}
-}, p = class extends s {
+}, p = class extends n {
 	constructor({ sender: e, to: t }) {
 		super("Reverted sender address does not match target contract address (`to`).", {
 			metaMessages: [`Contract address: ${t}`, `OffchainLookup sender address: ${e}`],
@@ -57,7 +57,7 @@ var d = class extends s {
 		}
 	]
 };
-async function g(e, { blockNumber: n, blockTag: r, data: s, to: u }) {
+async function g(e, { blockNumber: n, blockTag: i, data: s, to: u }) {
 	let { args: f } = t({
 		data: s,
 		abi: [h]
@@ -69,8 +69,8 @@ async function g(e, { blockNumber: n, blockTag: r, data: s, to: u }) {
 		});
 		let { data: t } = await c(e, {
 			blockNumber: n,
-			blockTag: r,
-			data: a([y, i([{ type: "bytes" }, { type: "bytes" }], [g.includes("x-batch-gateway:true") ? await l({
+			blockTag: i,
+			data: r([y, a([{ type: "bytes" }, { type: "bytes" }], [g.includes("x-batch-gateway:true") ? await l({
 				data: v,
 				ccipRequest: S
 			}) : await S({
@@ -92,46 +92,46 @@ async function g(e, { blockNumber: n, blockTag: r, data: s, to: u }) {
 		});
 	}
 }
-async function _({ data: t, sender: r, urls: i }) {
-	let a = /* @__PURE__ */ Error("An unknown error occurred.");
-	for (let o = 0; o < i.length; o++) {
-		let s = i[o], c = s.includes("{data}") ? "GET" : "POST", l = c === "POST" ? {
+async function _({ data: t, sender: n, urls: r }) {
+	let i = /* @__PURE__ */ Error("An unknown error occurred.");
+	for (let a = 0; a < r.length; a++) {
+		let o = r[a], c = o.includes("{data}") ? "GET" : "POST", l = c === "POST" ? {
 			data: t,
-			sender: r
+			sender: n
 		} : void 0, d = c === "POST" ? { "Content-Type": "application/json" } : {};
 		try {
-			let i = await fetch(s.replace("{sender}", r.toLowerCase()).replace("{data}", t), {
+			let r = await fetch(o.replace("{sender}", n.toLowerCase()).replace("{data}", t), {
 				body: JSON.stringify(l),
 				headers: d,
 				method: c
-			}), o;
-			if (o = i.headers.get("Content-Type")?.startsWith("application/json") ? (await i.json()).data : await i.text(), !i.ok) {
-				a = new u({
+			}), a;
+			if (a = r.headers.get("Content-Type")?.startsWith("application/json") ? (await r.json()).data : await r.text(), !r.ok) {
+				i = new u({
 					body: l,
-					details: o?.error ? e(o.error) : i.statusText,
-					headers: i.headers,
-					status: i.status,
-					url: s
+					details: a?.error ? e(a.error) : r.statusText,
+					headers: r.headers,
+					status: r.status,
+					url: o
 				});
 				continue;
 			}
-			if (!n(o)) {
-				a = new f({
-					result: o,
-					url: s
+			if (!s(a)) {
+				i = new f({
+					result: a,
+					url: o
 				});
 				continue;
 			}
-			return o;
+			return a;
 		} catch (e) {
-			a = new u({
+			i = new u({
 				body: l,
 				details: e.message,
-				url: s
+				url: o
 			});
 		}
 	}
-	throw a;
+	throw i;
 }
 //#endregion
 export { g as offchainLookup, m as offchainLookupSignature };
